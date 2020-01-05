0072764fc4c6be3da83bfa125bb50630502d99ea
Aliaksei Kuchyn <forrana@gmail.com>
2020-01-05 22:22:34 +0200

    Generate leds by matrix, add play possibility

--- scripts.js
+++ scripts.js
@@ -1,12 +1,39 @@
 var app = new Vue({
   el: '#app',
   data: {
-    
+    frames: [
+      [["#ff0000", "#00ff00", "#0000ff"], ["#000000", "#000000", "#000000"]],
+      [["#000000", "#ff0000", "#00ff00"], ["#0000ff", "#000000", "#000000"]],
+      [["#000000", "#000000", "#ff0000"], ["#00ff00", "#0000ff", "#000000"]],
+      [["#000000", "#000000", "#000000"], ["#ff0000", "#00ff00", "#0000ff"]],
+    ],
+    delay: 500,
+    intervalId: null,
+    activeFrameIndex: 0
   },
   computed: {
-
+    activeFrame: function () {
+      return this.frames[this.activeFrameIndex];
+    },
+    isPlaying: function () {
+      return this.intervalId !== null;
+    }
   },
   methods: {
-
-  }
+    startEmulation: function () {
+      this.intervalId = setInterval(() => {
+        this.activeFrameIndex = this.frames.length - 1 > this.activeFrameIndex ?
+          this.activeFrameIndex + 1 : 0;
+      }, this.delay);
+    },
+    pauseEmulation: function () {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    },
+    stopEmulation: function () {
+      this.activeFrameIndex = 0;
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  },
 })
